76ec24f3999a51aff5a3702e882413ecd72d8cb2
pdiercks <philipp.diercks@gmail.com>
2020-12-14 17:40:10 +0100

    add improved dofmap and test

--- dofmap.py
+++ dofmap.py
@@ -1,26 +1,38 @@
-"""DofMap to handle coarse and fine scale DoFs"""
-
 import logging
+from pathlib import Path
 import numpy as np
-import matplotlib.pyplot as plt
 import meshio
-from scipy.sparse import coo_matrix, csr_matrix
+from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import reverse_cuthill_mckee
-
-
-
 
 
 def adjacency_graph(cells, cell_type="quad"):
     N = np.unique(cells).size  # number of points
     r = np.zeros((N, N), dtype=int)
 
-    if cell_type == "quad":
+    if cell_type == "quad8":
+        local_adj = {
+            0: (4, 7),
+            1: (4, 5),
+            2: (5, 6),
+            3: (6, 7),
+            4: (0, 1),
+            5: (1, 2),
+            6: (2, 3),
+            7: (0, 3),
+        }
+    elif cell_type == "quad":
         local_adj = {
             0: (1, 3),
             1: (2, 0),
             2: (3, 1),
             3: (0, 2),
+        }
+    elif cell_type == "line3":
+        local_adj = {
+            0: (2, ),
+            1: (2, ),
+            2: (1, 0),
         }
     else:
         raise NotImplementedError
@@ -32,41 +44,69 @@ def adjacency_graph(cells, cell_type="quad"):
     return r
 
 
-# FIXME is this still up to date?
-# this code uses parts of the method msh_to_xdmf() from the dolfiny project
-# see here https://github.com/michalhabera/dolfiny/blob/master/dolfiny/mesh.py
+class Quadrilateral:
+    """
+    v3--e2--v2
+    |       |
+    e3      e1
+    |       |
+    v0--e0--v1
+    """
+    def __init__(self):
+        self.verts = ((0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0))
+        self.edges = {0: (0, 1), 1: (1, 2), 2: (2, 3), 3: (3, 1)}
+        self.faces = {0: (0, 1, 2, 3)}
+        self.topology = {0: {0: (0,), 1: (1,), 2: (2,), 3: (3,)}, 1: self.edges, 2: self.faces}
+
+    def set_entity_dofs(self, dofs_per_vert, dofs_per_edge, dofs_per_face):
+        n_vertex_dofs = len(self.verts) * dofs_per_vert
+        n_edge_dofs = len(self.edges) * dofs_per_edge
+        #  n_face_dofs = len(self.faces) * dofs_per_face
+        self.entity_dofs = {0: {}, 1: {}, 2: {}}
+        for v in range(len(self.verts)):
+            self.entity_dofs[0][v] = [dofs_per_vert * v + i for i in range(dofs_per_vert)]
+        for e in range(len(self.edges)):
+            self.entity_dofs[1][e] = [n_vertex_dofs + dofs_per_edge * e + i for i in range(dofs_per_edge)]
+        for f in range(len(self.faces)):
+            self.entity_dofs[2][f] = [n_edge_dofs + dofs_per_face * f + i for i in range(dofs_per_face)]
+
+
 class DofMap:
-    """class representing a DofMap of a lagrange function space where each vertex
-    is associated with gdim (or 1 if scalar) DoFs. In addition a up to 'modes'
-    DoFs are defined for each cell (center), representing the bubble modes.
+    """class representing a DofMap of a function space where each entity
+    (vertex, edge, face in 2d) is associated with the given number of DoFs,
+    when calling ``distribute_dofs()`.
 
     Parameters
     ----------
     mshfile
-        Name of .msh file (incl. extension).
+        Filepath (incl. ext) or instance of `meshio._mesh.Mesh`.
     tdim : int
         Topological dimension of the mesh.
-    gdim : int, optional
+    gdim : int
         Geometrical dimension of the mesh.
-    scalar : bool, optional
-        Whether the function space is scalar or not.
     prune_z_0 : bool, optional
         If True, prune zero z component.
+    cell : optional
+        Reference cell type.
     rcm : bool, optional
-        If True, reorder the vertices by use of the Reverse Cuthill McKee algorithm.
+        Reorder points using the Reverse Cuthill McKee algorithm.
 
     """
 
     def __init__(
-        self, mshfile, tdim, gdim=2, scalar=False, modes=0, prune_z_0=False, rcm=False
+        self, mshfile, tdim, gdim, prune_z_0=True, cell=Quadrilateral, rcm=False
     ):
-        logger = logging.getLogger("DofMap")
-
-        logger.info("Reading Gmsh mesh into meshio from path {}".format(mshfile))
-        mesh = meshio.read(mshfile)
-
         if not meshio.__version__ == "4.3.1":
             raise NotImplementedError
+        logger = logging.getLogger("DofMap")
+
+        if isinstance(mshfile, (str, Path)):
+            logger.info("Reading Gmsh mesh into meshio from path {}".format(mshfile))
+            mesh = meshio.read(mshfile)
+        elif isinstance(mshfile, meshio._mesh.Mesh):
+            mesh = mshfile
+        else:
+            raise TypeError
 
         if prune_z_0:
             mesh.prune_z_0()
@@ -118,57 +158,83 @@ class DofMap:
         else:
             self.points = points_pruned
             self.cells = subdomains_cells
-
+        self.tdim = tdim
         self.gdim = gdim
-        self.scalar = scalar
         self.cell_type = subdomains_celltype
-        self.modes = modes
+        self._cell = cell()
 
-    @property
-    def modes(self):
-        return self._modes
+    def distribute_dofs(self, dofs_per_vert, dofs_per_edge, dofs_per_face=0):
+        """set number of DoFs per entity and distribute dofs
 
-    @modes.setter
-    def modes(self, modes_per_edge):
-        if modes_per_edge < 0:
-            raise ValueError
-        self._modes = int(modes_per_edge)
+        Parameters
+        ----------
+        dofs_per_vert : int
+            Number of DoFs per vertex.
+        dofs_per_edge : int
+            Number of DoFs per edge
+        dofs_per_face : int, optional
+            Number of DoFs per face.
+        """
+        self._cell.set_entity_dofs(dofs_per_vert, dofs_per_edge, dofs_per_face)
+        entity_dofs = self._cell.entity_dofs
+        # FIXME assumes cell type "quad8"
+        dimension = list(range(self.tdim + 1))
+        x_dofs = []
+        self._dm = {dim: {} for dim in dimension}
+        DoF = 0
+        for ci, cell in enumerate(self.cells):
+            for dim in dimension:
+                if dim == 0:
+                    entities = cell[:4]
+                elif dim == 1:
+                    entities = cell[4:]
+                else:
+                    entities = [ci]
+                for local_ent, ent in enumerate(entities):
+                    if ent not in self._dm[dim].keys():
+                        self._dm[dim][ent] = []
+                        dofs = entity_dofs[dim][local_ent]
+                        for dof in dofs:
+                            x_dofs.append(self.points[ent])  # store dof coordinates
+                            self._dm[dim][ent].append(DoF)
+                            DoF += 1
+        self.n_dofs = DoF
+        self.dofs_per_vert = dofs_per_vert
+        self.dofs_per_edge = dofs_per_edge
+        self.dofs_per_face = dofs_per_face
+        self._x_dofs = np.array(x_dofs)
+
+    def tabulate_dof_coordinates(self):
+        """return dof coordinates"""
+        if not hasattr(self, "_x_dofs"):
+            raise AttributeError("You need to distribute DoFs first")
+        return self._x_dofs
 
     def dofs(self):
         """return total number of dofs"""
-        return self.vertex_dofs() + self.bubble_dofs()
+        if not hasattr(self, "n_dofs"):
+            raise AttributeError("You need to distribute DoFs first")
+        return self.n_dofs
 
-    def vertex_dofs(self):
-        """return number of all vertex dofs"""
-        dim = 1 if self.scalar else self.gdim
-        number_vertices = self.points.shape[0]
-        return number_vertices * dim
+    def cell_dofs(self, cell_index):
+        if not hasattr(self, "_dm"):
+            raise AttributeError("You need to distribute DoFs first")
+        dimension = list(range(self.tdim + 1))
+        cell = self.cells[cell_index]
+        cell_dofs = []
+        for dim in dimension:
+            if dim == 0:
+                entities = cell[:4]
+            elif dim == 1:
+                entities = cell[4:]
+            else:
+                entities = [cell_index]
+            for ent in entities:
+                cell_dofs += self._dm[dim][ent]
+        return cell_dofs
 
-    def bubble_dofs(self):
-        """return max number of all bubble dofs"""
-        dim = self.modes
-        number_cells = self.cells.shape[0]
-        return number_cells * dim
-
-    def cell_vertex_dofs(self, cell_index):
-        """return vertex dofs for cell"""
-        dim = 1 if self.scalar else self.gdim
-        vertices = self.cells[cell_index]
-        dofs = []
-        for v in vertices:
-            j = v * dim
-            for component in range(dim):
-                dofs.append(j + component)
-        return np.array(dofs)
-
-    def cell_bubble_dofs(self, cell_index):
-        """return bubble dofs for cell"""
-        vertex_dofs = self.vertex_dofs()
-        dofs = np.arange(self.modes) + cell_index * self.modes + vertex_dofs
-        return dofs
-
-    def locate_vertex_dofs(self, X, sub=None, tol=1e-9):
-        """returns dofs at vertex coordinates X
+    def locate_dofs(self, X, sub=None, tol=1e-9):
+        """returns dofs at coordinates X
 
         Parameters
         ----------
@@ -177,28 +243,29 @@ class DofMap:
         sub : int, optional
             Index of component.
         tol : float, optional
-            Tolerance used to find vertex coordinate.
+            Tolerance used to find coordinate.
 
         Returns
         -------
         dofs : list
-            DoFs at given vertices.
+            DoFs at given coordinates.
         """
         if isinstance(X, list):
             X = np.array(X).reshape(len(X), self.gdim)
         assert isinstance(X, np.ndarray)
-        dim = 1 if self.scalar else self.gdim
 
         dofs = []
         for x in X:
-            p = np.abs(self.points - x)
+            p = np.abs(self._x_dofs - x)
             v = np.where(np.all(p < tol, axis=1))[0]
             if v.size < 1:
                 raise IndexError(f"The point {x} is not a vertex of the grid!")
-            for d in range(dim):
-                dofs.append(dim * v[0] + d)
+            dofs += v.tolist()
 
         if sub is not None:
+            # FIXME user has to know that things might go wrong if
+            # X contains vertex AND edge coordinates ...
+            dim = v.size
             return dofs[sub::dim]
         else:
             return dofs
