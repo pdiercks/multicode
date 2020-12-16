83d2291b22f161a597c687276c519b0dd8f569f9
pdiercks <philipp.diercks@gmail.com>
2020-12-16 15:00:36 +0100

    add test for mapping

---
+++ multi/test/test_make_mapping.py
@@ -0,0 +1,19 @@
+"""test make mapping"""
+import dolfin as df
+from multi import make_mapping, Domain
+from numpy import allclose
+
+
+def test():
+    domain = Domain("data/rvedomain.xdmf", edges=True)
+    V = df.FunctionSpace(domain.mesh, "CG", 2)
+    L = df.FunctionSpace(domain.edges[0], "CG", 2)
+    V_to_L = make_mapping(L, V)
+
+    x_dofs = V.tabulate_dof_coordinates()
+    bottom = L.tabulate_dof_coordinates()
+    assert allclose(bottom, x_dofs[V_to_L])
+
+
+if __name__ == "__main__":
+    test()
