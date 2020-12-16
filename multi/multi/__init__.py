4c1570abff13d56ea3d25152289afac3e3b62bea
pdiercks <philipp.diercks@gmail.com>
2020-12-16 14:51:27 +0100

    add make mapping

--- multi/multi/__init__.py
+++ multi/multi/__init__.py
@@ -1,7 +1,8 @@
 name = "multi"
+from multi.bcs import MechanicsBCs
 from multi.dofmap import DofMap
 from multi.domain import Domain
-from multi.product import InnerProduct
-from multi.bcs import MechanicsBCs
-from multi.linear_elasticity import LinearElasticMaterial, LinearElasticityProblem
 from multi.io import ResultFile
+from multi.linear_elasticity import LinearElasticMaterial, LinearElasticityProblem
+from multi.misc import make_mapping
+from multi.product import InnerProduct
