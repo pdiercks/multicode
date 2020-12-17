d15cfe9b92f14e4e95c979b7aca949243b06cb3c
pdiercks <philipp.diercks@gmail.com>
2020-12-17 15:29:01 +0100

    add tasks for meshes

--- example/dodo.py
+++ example/dodo.py
@@ -1,5 +1,6 @@
 """task definition for test example"""
 
+from doit import create_after
 from doit.action import CmdAction
 from doit.tools import run_once
 from pathlib import Path
@@ -7,12 +8,12 @@ import yaml
 
 example = Path(__file__).parent
 preprocessing = example.absolute().parent / "preprocessing"
-scenarios = [example / "scenarios.yml"]
+scenarios = example / "scenarios.yml"
 
 
 def task_create_scenarios():
-    """create all scenarios for this example, where each
-    scenario consists of discretization, degree and basis type"""
+    """create all scenarios for this example,
+    where each scenario consists of discretization, degree and basis type"""
 
     def create_scenarios(targets):
         # discretization with 5 points per edge
@@ -31,28 +32,58 @@ def task_create_scenarios():
 
     return {
         "actions": [create_scenarios],
-        "targets": scenarios,
+        "targets": [scenarios],
         "uptodate": [run_once],
-        "verbosity": 2,
     }
 
 
+@create_after(executed="create_scenarios")
 def task_rve_grid():
     """create RVE fine grid"""
     script = preprocessing / "rve_type_01.py"
-    with open(scenarios[0], "r") as instream:
+    with open(scenarios, "r") as instream:
         s = yaml.safe_load(instream)
 
     for i, scenario in s.items():
-        targets = [example / f"rve_{i}.xdmf"]
+        target = example / f"rve_{i}.xdmf"
         N = int((scenario["disc"] + 1) / 2)
-        cmd = f"python {script} 0 0 1 1 0.2 {N} --output={targets[0]}"
+        cmd = f"python {script} 0 0 1 1 0.2 {N} --output={target}"
         yield {
             "name": f"{i}",
-            "file_dep": scenarios + [script],
+            "file_dep": [scenarios, script],
             "actions": [CmdAction(cmd)],
-            "targets": targets,
+            "targets": [target, target.with_suffix(".h5")],
             "clean": True,
+            "task_dep": ["create_scenarios"],
+            "verbosity": 2,
+        }
+
+
+@create_after(executed="rve_metadata")
+def task_rve_edges():
+    """create meshes for the boundary of the RVE"""
+    script = preprocessing / "rve_edges.py"
+    edges = ["bottom", "right", "top", "left"]
+    metadata = example / "rve.yml"
+
+    with open(metadata, "r") as md:
+        rvemd = yaml.safe_load(md)
+    with open(scenarios, "r") as instream:
+        s = yaml.safe_load(instream)
+
+    for i, scenario in s.items():
+        N = rvemd[i]["disc"]  # unit length possibly depending on scenario
+        rve = Path(rvemd[i]["xdmf"])
+        # note target definition according to rve_edges.py
+        targets = [rve.parent / (rve.stem + f"_{e}.xdmf") for e in edges]
+        cmd = f"python {script} {rve} --transfinite={N}"
+        yield {
+            "name": f"{i}",
+            "file_dep": [script, metadata, scenarios, rve],
+            "actions": [CmdAction(cmd)],
+            "targets": targets + [t.with_suffix(".h5") for t in targets],
+            "clean": True,
+            "verbosity": 2,
         }
 
 
@@ -61,7 +92,7 @@ def task_rve_metadata():
     targets = [example / "rve.yml"]
 
     def create_metadata(targets):
-        with open(scenarios[0], "r") as instream:
+        with open(scenarios, "r") as instream:
             s = yaml.safe_load(instream)
 
         r = {}
@@ -72,10 +103,54 @@ def task_rve_metadata():
             yaml.safe_dump(r, out)
 
     return {
+        "file_dep": [scenarios],
         "targets": targets,
         "actions": [create_metadata],
         "uptodate": [run_once],
-        "verbosity": 2,
+    }
+
+
+@create_after(executed="rve_metadata")
+def task_coarse_grid():
+    """create coarse grid for this example which is a 3x3 block of RVEs"""
+    script = preprocessing / "rectangle.py"
+    metadata = example / "rve.yml"
+
+    with open(metadata, "r") as md:
+        rvemd = yaml.safe_load(md)
+
+    N = 3
+    for i, rve in rvemd.items():
+        target = example / f"coarse_block_{i}.msh"
+        x = rve["a"] * N
+        cmd = f"python {script} {x} {x} {N} {N} 1 --transfinite --quads"
+        cmd += f" --output={target}"
+        return {
+            "file_dep": [script, metadata],
+            "actions": [CmdAction(cmd)],
+            "targets": [target],
+            "clean": True,
         }
 
 
+@create_after(executed="create_scenarios")
+def task_fine_grid():
+    """create fine grid for global structure"""
+    script = preprocessing / "soi.py"
+
+    with open(scenarios, "r") as instream:
+        s = yaml.safe_load(instream)
+
+    for i, scenario in s.items():
+        coarse_grid = example / f"coarse_block_{i}.msh"
+        rve = example / f"rve_{i}.xdmf"
+        fine_grid = example / f"block_{i}.xdmf"
+        cmd = f"python {script} {coarse_grid} {rve} --tdim=2 --gdim=2 --prune_z_0"
+        cmd += f" --output={fine_grid}"
+        yield {
+            "name": f"{i}",
+            "file_dep": [script, coarse_grid, rve, scenarios],
+            "actions": [CmdAction(cmd)],
+            "targets": [fine_grid, fine_grid.with_suffix(".h5")],
+            "clean": True,
+        }
---
+++ preprocessing/rve_edges.py
@@ -0,0 +1,88 @@
+"""
+generate edge meshes for given RVE mesh
+
+Usage:
+    rve_edges.py [options] RVE
+
+Arguments:
+    RVE       Filepath (incl. .xdmf extension).
+
+Options:
+    -h, --help               Show this message and exit.
+    --transfinite=N          Use N equidistant points on each edge.
+    --lcar=LCAR              Specify characteristic length.
+    -o FILE, --output=FILE   Optional filepath such that edge meshes
+                             are written as FILE_{edge}.xdmf. Otherwise
+                             FILE=Path(RVE).
+"""
+
+import sys
+from pathlib import Path
+from docopt import docopt
+
+import meshio
+import pygmsh
+
+from multi import Domain
+
+
+def parse_arguments(args):
+    args = docopt(__doc__, args)
+    args["RVE"] = Path(args["RVE"])
+    return args
+
+
+def main(args):
+    args = parse_arguments(args)
+    domain = Domain(args["RVE"], id_=0, subdomains=True, edges=False)
+    points = {
+        "bottom": ([domain.xmin, domain.ymin], [domain.xmax, domain.ymin]),
+        "right": ([domain.xmax, domain.ymin], [domain.xmax, domain.ymax]),
+        "top": ([domain.xmin, domain.ymax], [domain.xmax, domain.ymax]),
+        "left": ([domain.xmin, domain.ymin], [domain.xmin, domain.ymax]),
+    }
+    for edge, (start, end) in points.items():
+        create_edge_mesh(args, start, end, edge)
+
+
+def create_edge_mesh(args, X, Y, edge):
+    """create partition of a line in two space dimensions
+
+    Parameters
+    ----------
+    X : tuple, list of floats
+        Start point of the line.
+    Y : tuple, list of floats
+        End point of the line.
+    edge : str
+        The edge label.
+    """
+    geom = pygmsh.built_in.Geometry()
+
+    if args["--lcar"]:
+        lcar = float(args["--lcar"])
+    else:
+        lcar = None
+
+    p0 = geom.add_point([X[0], X[1], 0.0], lcar=lcar)
+    p1 = geom.add_point([Y[0], Y[1], 0.0], lcar=lcar)
+
+    l = geom.add_line(p0, p1)
+    if args["--transfinite"]:
+        N = int(args["--transfinite"])
+        geom.set_transfinite_lines([l], size=N)
+
+    # add physical such that mesh only contains one cell type
+    geom.add_physical(l, label="line")
+
+    mesh = pygmsh.generate_mesh(geom)
+    mesh.prune_z_0()
+    if args["--output"]:
+        xdmf = args["--output"].parent / (args["--output"].stem + f"_{edge}.xdmf")
+    else:
+        xdmf = args["RVE"].parent / (args["RVE"].stem + f"_{edge}.xdmf")
+    meshio.write(xdmf, meshio.Mesh(points=mesh.points, cells=mesh.cells))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
