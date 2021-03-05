dd99774a97ab589fd31cb69a96f5ed81162a5464
pdiercks <philipp.diercks@bam.de>
2021-03-05 19:07:53 +0100

    [wip] add plot edge basis

---
+++ postprocessing/plot_edge_basis.py
@@ -0,0 +1,69 @@
+"""
+plot given modes of the edge basis functions
+
+Usage:
+    plot_edge_basis.py [options] RVE DEG CHI MODES...
+
+Arguments:
+    RVE          The rve grid used.
+    DEG          The degree of FE space.
+    CHI          TODO
+    MODES        TODO
+
+Options:
+    -h, --help     Show this message and exit.
+    --set=SET      Bottom-Top (0) or Right-Left (1) set. [default: 0]
+    --output=PDF   Save to filepath PDF.
+"""
+
+import sys
+from pathlib import Path
+from docopt import docopt
+import numpy as np
+import matplotlib.pyplot as plt
+from dolfin import FunctionSpace
+from multi import Domain
+
+
+def parse_arguments(args):
+    args = docopt(__doc__, args)
+    args["RVE"] = Path(args["RVE"])
+    args["DEG"] = int(args["DEG"])
+    args["CHI"] = Path(args["CHI"])
+    args["MODES"] = [int(m) for m in args["MODES"]]
+    args["--set"] = str(args["--set"])
+    args["--output"] = Path(args["--output"]) if args["--output"] is not None else None
+    return args
+
+
+def main(args):
+    args = parse_arguments(args)
+    domain = Domain(args["RVE"], 0, subdomains=True, edges=True)
+    s = args["--set"]
+    edge = domain.edges[int(s)]
+    V = FunctionSpace(edge, "CG", args["DEG"])
+    x_dofs = V.tabulate_dof_coordinates()[:, int(s)]
+    o = np.argsort(x_dofs)
+    basis = np.load(args["CHI"])[s]
+
+    if args["--output"] is None:
+        # plot without PlottingContext
+        fig, axs = plt.subplots(1, 2)
+        axs = axs.ravel()
+        for i in range(2):
+            for m in args["MODES"]:
+                axs[i].plot(x_dofs[o], basis[m][i::2][o], "-x")
+
+        plt.tight_layout()
+        plt.show()
+    else:
+        raise NotImplementedError
+        from plotstuff import PlottingContext
+
+        plot_argv = [__file__, args["--output"]]
+        with PlottingContext(plot_argv, "pdiercks_multi") as fig:
+            ax = plt.axes()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
