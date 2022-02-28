417726cf602fe065c5dc5dfb60e8d447ebb97d2f
pdiercks <philipp.diercks@bam.de>
2022-02-28 18:48:31 +0100

    [multi][misc] add set_zero_at_dofs and locate_dofs

--- multi/multi/misc.py
+++ multi/multi/misc.py
@@ -356,3 +356,54 @@ def select_modes(basis, modes_per_edge, max_modes):
     left = [offset + i for i in range(modes_per_edge)]
     ind = coarse + bottom + right + top + left
     return basis[ind]
+
+
+def set_zero_at_dofs(U, dofs, atol=1e-6):
+    """set U to zero at dofs if within relaxed tolerance"""
+    zero = U.dofs(dofs)
+    if np.allclose(np.zeros_like(zero), zero, atol=atol):
+        # set respective dofs to zero
+        # this modifies U._data in-place
+        array = U.to_numpy()
+        array[:, dofs] = np.zeros_like(zero)
+
+
+def locate_dofs(x_dofs, X, gdim=2, s_=np.s_[:], tol=1e-9):
+    """returns dofs at coordinates X
+
+    Parameters
+    ----------
+    x_dofs : np.ndarray
+        An array containing the coordinates of the DoFs of the FE space.
+        Most likely the return value of V.tabulate_dof_coordinates().
+    X : list, np.ndarray
+        A list of points, where each point is given as list of len(gdim).
+    gdim : int, optional
+        The geometrical dimension of the domain.
+    s_ : slice, optional
+        Return slice of the dofs at each point.
+    tol : float, optional
+        Tolerance used to find coordinate.
+
+    Returns
+    -------
+    dofs : np.ndarray
+        DoFs at given coordinates.
+    """
+    if isinstance(X, list):
+        X = np.array(X).reshape(len(X), gdim)
+    elif isinstance(X, np.ndarray):
+        if X.ndim == 1:
+            X = X[np.newaxis, :]
+        elif X.ndim > 2:
+            raise NotImplementedError
+
+    dofs = np.array([], int)
+    for x in X:
+        p = np.abs(x_dofs - x)
+        v = np.where(np.all(p < tol, axis=1))[0]
+        if v.size < 1:
+            raise IndexError(f"The point {x} is not a vertex of the grid!")
+        dofs = np.append(dofs, v[s_])
+
+    return dofs
