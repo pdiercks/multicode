4e58cd095b3206daadc7c747f8e57bf93d1d6875
pdiercks <philipp.diercks@bam.de>
2022-02-28 18:42:16 +0100

    [multi][test][transfer op] remove comments

--- multi/test/test_transfer_operator.py
+++ multi/test/test_transfer_operator.py
@@ -60,39 +60,6 @@ def test():
     u_ref_in = df.interpolate(u_ref, R)
     err = np.array(u).flatten() - u_ref_in.vector()[:]
     assert np.linalg.norm(err) < 1e-6
-    # # transfer operator
-    # A = df.assemble(lhs_form)
-    # dummy = df.Function(V)
-    # bc_hom.zero_columns(A, dummy.vector(), 1.0)
-
-    # all_dofs = np.arange(V.dim())
-    # all_inner_dofs = np.setdiff1d(all_dofs, dofs_out)
-    # Amat = df.as_backend_type(A).mat()
-    # full_operator = csc_matrix(Amat.getValuesCSR()[::-1], shape=Amat.size)
-    # operator = full_operator[:, all_inner_dofs][all_inner_dofs, :]
-
-    # # factorization
-    # matrix_shape = operator.shape
-    # start = time.time()
-    # operator = factorized(operator)
-    # end = time.time()
-    # print(f"factorization of {matrix_shape} matrix in {end-start}")
-
-    # target_subdomain = TargetSubdomain()
-    # target_subdomain_mesh = df.SubMesh(mesh, target_subdomain)
-    # R = df.FunctionSpace(target_subdomain_mesh, V.ufl_element())
-    # V_to_R = make_mapping(R, V)
-
-    # # mapping from old to new dof numbers
-    # newdofs = np.zeros((V.dim(),), dtype=int)
-    # newdofs[all_inner_dofs] = np.arange(all_inner_dofs.size)
-    # range_dofs = newdofs[V_to_R]
-
-    # rhs_op = full_operator[:, dofs_out][all_inner_dofs, :]
-    # start = time.time()
-    # transfer_operator = -operator(rhs_op.todense())[range_dofs, :]
-    # end = time.time()
-    # print(f"applied operator to rhs in {end-start}")
 
 
 if __name__ == "__main__":
