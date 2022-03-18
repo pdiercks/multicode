eabc0b5a3e55664c8baf87349aa70b27cc2ebd80
pdiercks <philipp.diercks@bam.de>
2022-03-18 21:10:31 +0100

    [multi][basis construction] add compute phi which uses extend_pymor

--- multi/multi/basis_construction.py
+++ multi/multi/basis_construction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import dolfin as df
 
+from multi.extension import extend_pymor
 from multi.shapes import NumpyQuad
 from multi.misc import locate_dofs
 
@@ -9,7 +10,6 @@ from scipy.special import erfinv
 
 from pymor.algorithms.gram_schmidt import gram_schmidt
 from pymor.bindings.fenics import (
-    FenicsVectorSpace,
     FenicsMatrixOperator,
     FenicsVisualizer,
 )
@@ -18,6 +18,24 @@ from pymor.operators.constructions import LincombOperator, VectorOperator
 from pymor.operators.interface import Operator
 from pymor.parameters.functionals import ProjectionParameterFunctional
 from pymor.vectorarrays.interface import VectorArray
+
+
+def compute_phi(problem, solver_options=None):
+    # just a wrapper around extend
+    # only need to define the boundary data
+    V = problem.V
+    n_vertices = 4  # assumes rectangular domain
+    nodes = problem.domain.get_nodes(n=n_vertices)
+    quadrilateral = NumpyQuad(nodes)
+    shape_functions = quadrilateral.interpolate(V)
+
+    bc = df.DirichletBC(V, df.Function(V), df.DomainBoundary())
+    bc_dofs = list(bc.get_boundary_values().keys())
+    boundary_data = np.zeros_like(shape_functions)
+    boundary_data[:, bc_dofs] = shape_functions[:, bc_dofs]
+
+    phi = extend_pymor(problem, boundary_data, solver_options=solver_options)
+    return phi
 
 
 def construct_coarse_scale_basis(problem, solver_options=None, return_fom=False):
@@ -52,7 +70,7 @@ def construct_coarse_scale_basis(problem, solver_options=None, return_fom=False)
     shape_functions = quadrilateral.interpolate(V)
 
     null = np.zeros(V.dim())
-    space = FenicsVectorSpace(V)
+    # source = problem.source
     vector_operators = []
     for shape in shape_functions:
         f.vector().set_local(null)
@@ -60,7 +78,9 @@ def construct_coarse_scale_basis(problem, solver_options=None, return_fom=False)
         bc = df.DirichletBC(V, g, df.DomainBoundary())
         A_bc = A.copy()
         bc.zero_columns(A_bc, f.vector(), 1.0)
-        vector_operators.append(VectorOperator(space.make_array([f.vector().copy()])))
+        vector_operators.append(
+            VectorOperator(problem.range.make_array([f.vector().copy()]))
+        )
 
     lhs = FenicsMatrixOperator(A_bc, V, V, solver_options=solver_options)
     parameter_functionals = [
@@ -92,7 +112,7 @@ def construct_coarse_scale_basis(problem, solver_options=None, return_fom=False)
             "h1_0": FenicsMatrixOperator(h1_0_mat, V, V, name="h1_0"),
         },
         error_estimator=None,
-        visualizer=FenicsVisualizer(space),
+        visualizer=FenicsVisualizer(problem.range),
         name="FOM",
     )
 
@@ -108,6 +128,155 @@ def construct_coarse_scale_basis(problem, solver_options=None, return_fom=False)
         return basis, fom
     else:
         return basis
+
+
+# adapted from pymor.algorithms.randrangefinder.adaptive_rrf
+# to be used with multi.oversampling.OversamplingProblem
+def _compute_fine_scale_snapshots_os(
+    oversampling_problem,
+    coarse_scale_basis,
+    source_product=None,
+    range_product=None,
+    tol=1e-4,
+    failure_tolerance=1e-15,
+    num_testvecs=20,
+    lambda_min=None,
+    train_vectors=None,
+):
+    r"""Adaptive randomized range approximation of `A`.
+    This is an implementation of Algorithm 1 in [BS18]_.
+
+    The following modifications where made:
+        - instead of a transfer operator A, the full oversampling
+        problem is used
+        - optional training data was added (`train_vectors`)
+
+    The image Av = A.apply(v) is equivalent to the restriction
+    of the full solution to the target domain Ω_in, i.e.
+        U = oversampling_problem.solve(v)
+    See multi.oversampling.OversamplingProblem.solve.
+
+    Given the |Operator| `A`, the return value of this method is the |VectorArray|
+    `B` with the property
+
+    .. math::
+        \Vert A - P_{span(B)} A \Vert \leq tol
+
+    with a failure probability smaller than `failure_tolerance`, where the norm denotes the
+    operator norm. The inner product of the range of `A` is given by `range_product` and
+    the inner product of the source of `A` is given by `source_product`.
+
+    Parameters
+    ----------
+    oversampling_problem
+        The full oversampling problem associated with a (transfer) |Operator| A.
+    coarse_scale_basis
+        The |VectorArray| containing the coarse scale basis.
+    source_product
+        Inner product |Operator| of the source of A.
+    range_product
+        Inner product |Operator| of the range of A.
+    tol
+        Error tolerance for the algorithm.
+    failure_tolerance
+        Maximum failure probability.
+    num_testvecs
+        Number of test vectors.
+    lambda_min
+        The smallest eigenvalue of source_product.
+        If `None`, the smallest eigenvalue is computed using scipy.
+    train_vectors
+        |VectorArray| containing a set of predefined training
+        vectors.
+
+    Returns
+    -------
+    B
+        |VectorArray| which contains fine scale part of the computed solutions
+        of the oversampling problem.
+    """
+    problem = oversampling_problem
+
+    assert source_product is None or isinstance(source_product, Operator)
+    assert range_product is None or isinstance(range_product, Operator)
+    assert (
+        train_vectors is None
+        or isinstance(train_vectors, VectorArray)
+        and train_vectors.space is problem.source
+    )
+    assert coarse_scale_basis.space is problem.range
+
+    if source_product is None:
+        lambda_min = 1
+    elif lambda_min is None:
+
+        def mv(v):
+            return source_product.apply(source_product.source.from_numpy(v)).to_numpy()
+
+        def mvinv(v):
+            return source_product.apply_inverse(
+                source_product.range.from_numpy(v)
+            ).to_numpy()
+
+        L = LinearOperator(
+            (source_product.source.dim, source_product.range.dim), matvec=mv
+        )
+        Linv = LinearOperator(
+            (source_product.range.dim, source_product.source.dim), matvec=mvinv
+        )
+        lambda_min = eigsh(
+            L, sigma=0, which="LM", return_eigenvectors=False, k=1, OPinv=Linv
+        )[0]
+
+    # NOTE problem.source is the full space, while the source product is of lower dimension
+    num_source_dofs = len(problem._bc_dofs_gamma_out)
+    testfail = failure_tolerance / min(num_source_dofs, problem.range.dim)
+    testlimit = (
+        np.sqrt(2.0 * lambda_min) * erfinv(testfail ** (1.0 / num_testvecs)) * tol
+    )
+    maxnorm = np.inf
+    # set of test vectors
+    R = problem.generate_random_boundary_data(num_testvecs, distribution="normal")
+    M = problem.solve(R)
+
+    # get vertex dofs of target subdomain
+    subdomain = problem.subdomain_problem.domain
+    vertices = subdomain.get_nodes(n=4)  # assumes rectangular domain
+    vertex_dofs = locate_dofs(problem.range.V.tabulate_dof_coordinates(), vertices)
+
+    B = problem.range.empty()
+    snapshots = problem.range.empty()
+    while maxnorm > testlimit:
+        basis_length = len(B)
+        if train_vectors is not None and basis_length < len(train_vectors):
+            v = train_vectors[basis_length]
+        else:
+            v = problem.generate_random_boundary_data(count=1, distribution="normal")
+
+        # subtract coarse scale part
+        r = problem.solve(v)
+        nodal_values = r.dofs(vertex_dofs)
+        r -= coarse_scale_basis.lincomb(nodal_values)
+        snapshots.append(r)
+
+        B.append(problem.solve(v))
+        gram_schmidt(
+            B,
+            range_product,
+            atol=0,
+            rtol=0,
+            offset=basis_length,
+            copy=False,
+        )
+        M -= B.lincomb(B.inner(M, range_product).T)
+        maxnorm = np.max(M.norm(range_product))
+
+    zero_at_dofs = np.allclose(
+        snapshots.to_numpy()[:, vertex_dofs], np.zeros(vertex_dofs.size)
+    )
+    assert zero_at_dofs
+
+    return snapshots
 
 
 def compute_fine_scale_snapshots(
