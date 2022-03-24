65689fa0992db4acc00fb0fb3b68f9ef0d7764a8
pdiercks <philipp.diercks@bam.de>
2022-03-24 16:48:45 +0100

    update __init__; delete/comment deprecated code

--- multi/multi/__init__.py
+++ multi/multi/__init__.py
@@ -3,6 +3,7 @@ from multi.bcs import BoundaryConditions, apply_bcs
 from multi.dofmap import DofMap
 from multi.domain import Domain, RectangularDomain
 from multi.io import ResultFile
-from multi.linear_elasticity import LinearElasticMaterial, LinearElasticityProblem
+from multi.materials import LinearElasticMaterial
+from multi.problems import LinearElasticityProblem
 from multi.misc import make_mapping
 from multi.product import InnerProduct
