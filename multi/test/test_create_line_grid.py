de55681ea63060b3df7b42c497cca8d319289ead
Philipp Diercks <philipp.diercks@bam.de>
2022-09-12 17:43:51 +0200

    extend test for line grid creation

--- multi/test/test_create_line_grid.py
+++ multi/test/test_create_line_grid.py
@@ -3,9 +3,26 @@ from multi.preprocessing import create_line_grid
 
 if __name__ == "__main__":
     num_cells = 10
-    # FIXME should be able to use arbitrary start and end points and still get num_cells cells
-    mesh = create_line_grid([1.2, 0.1, 0], [0.2, 1, 0], num_cells=num_cells)
-    from IPython import embed; embed()
+    mesh = create_line_grid([0, 0, 0], [1, 0, 0], num_cells=num_cells)
     assert mesh.points.shape == (num_cells+1, 3)
     assert mesh.get_cells_type("line").shape[0] == num_cells
 
+    mesh = create_line_grid([0, 0, 0], [-1, 0, 0], num_cells=num_cells)
+    assert mesh.points.shape == (num_cells+1, 3)
+    assert mesh.get_cells_type("line").shape[0] == num_cells
+
+    mesh = create_line_grid([0, 0, 0], [0, 1, 0], num_cells=num_cells)
+    assert mesh.points.shape == (num_cells+1, 3)
+    assert mesh.get_cells_type("line").shape[0] == num_cells
+
+    mesh = create_line_grid([0.2, 0, 0], [1.2, 1, 0], num_cells=num_cells)
+    assert mesh.points.shape == (num_cells+1, 3)
+    assert mesh.get_cells_type("line").shape[0] == num_cells
+
+    mesh = create_line_grid([0.2, 0.5, 0], [-1.2, -0.3, 0], num_cells=num_cells)
+    assert mesh.points.shape == (num_cells+1, 3)
+    assert mesh.get_cells_type("line").shape[0] == num_cells
+
+    mesh = create_line_grid([1.2, 0.1, 0], [0.2, 1, 0], num_cells=num_cells)
+    assert mesh.points.shape == (num_cells+1, 3)
+    assert mesh.get_cells_type("line").shape[0] == num_cells
