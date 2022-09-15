895b88dc9f187bb0aaec3db3b66d8bd1f8e336c2
Philipp Diercks <philipp.diercks@bam.de>
2022-09-15 17:37:11 +0200

    [preprocessing] add rce grid type 01

--- multi/multi/preprocessing.py
+++ multi/multi/preprocessing.py
@@ -31,14 +31,28 @@ def to_array(values):
     return np.array(floats, dtype=float)
 
 
-def _write(filepath, mesh):
+# this code is part of the FEniCSx tutorial
+# see https://jorgensd.github.io/dolfinx-tutorial/chapter3/subdomains.html#convert-msh-files-to-xdmf-using-meshio
+def create_mesh(mesh, cell_type, prune_z=False):
+    cells = mesh.get_cells_type(cell_type)
+    cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
+    points = mesh.points[:,:2] if prune_z else mesh.points
+    # FIXME might change 'gmsh:physical' to something more meaningful?
+    # would need to change __init__ of multi.domain.Domain as well
+    out_mesh = meshio.Mesh(points=points, cells={cell_type: cells}, cell_data={"gmsh:physical":[cell_data]})
+    return out_mesh
+
+
+def _write(filepath, mesh, cell_type, prune_z=False):
     p = pathlib.Path(filepath)
     suffix = p.suffix
 
     if suffix == ".msh":
         meshio.write(p.as_posix(), mesh, file_format="gmsh")
     elif suffix == ".xdmf":
-        meshio.write(p.as_posix(), mesh, file_format="xdmf")
+        m = create_mesh(mesh, cell_type, prune_z=prune_z)
+        breakpoint()
+        meshio.write(p.as_posix(), m, file_format="xdmf")
     else:
         raise NotImplementedError
 
@@ -75,7 +89,7 @@ def create_line_grid(start, end, lc=0.1, num_cells=None, out_file=None):
 
     grid = _generate_and_read_grid(1)
     if out_file is not None:
-        _write(out_file, grid)
+        _write(out_file, grid, "line", prune_z=True)
 
     return grid
 
@@ -108,6 +122,7 @@ def create_rectangle_grid(
     curve_loop = gmsh.model.geo.addCurveLoop([l0, l1, l2, l3])
     surface = gmsh.model.geo.addPlaneSurface([curve_loop])
 
+    cell_type = "triangle"
     if num_cells is not None:
         try:
             nx, ny = num_cells
@@ -123,50 +138,135 @@ def create_rectangle_grid(
         if recombine:
             # setRecombine(dim, tag, angle=45.0)
             gmsh.model.geo.mesh.setRecombine(2, surface)
+            cell_type = "quad"
 
     gmsh.model.geo.synchronize()
     gmsh.model.addPhysicalGroup(2, [surface])
 
     grid = _generate_and_read_grid(2)
     if out_file is not None:
-        _write(out_file, grid)
+        _write(out_file, grid, cell_type, prune_z=True)
 
     return grid
 
 
-def create_rce_grid_01(xmin, xmax, ymin, ymax, z=0.0, radius=0.2, lc=0.1, num_cells_per_edge=None):
+def create_rce_grid_01(xmin, xmax, ymin, ymax, z=0.0, radius=0.2, lc=0.1, num_cells_per_edge=None, out_file=None):
     """TODO docstring"""
+
+    width = abs(xmax-xmin)
+    height = abs(ymax-ymin)
+
     gmsh.initialize()
     gmsh.model.add("rce_01")
+
+    # options
+    gmsh.option.setNumber("Mesh.Smoothing", 2)
 
     geom = gmsh.model.geo
 
     # add the inclusion (circle) as 8 circle arcs
-    phi = np.linspace(0, 2*np.pi, num=8, endpoint=True)
-    x_center = np.tile(np.array([xmin + (xmax-xmin)/2, ymin + (ymax-ymin)/2, z]), (8, 1))
-    x_unit_circle = np.array([np.cos(phi), np.sin(phi), np.zeros_like(phi)]).T
-    x_circle = x_center + x_unit_circle
+    phi = np.linspace(0, 2*np.pi, num=9, endpoint=True)[:-1]
+    x_center = np.array([xmin + width/2, ymin + height/2, z])
+    x_unit_circle = np.array([radius * np.cos(phi), radius * np.sin(phi), np.zeros_like(phi)]).T
+    x_circle = np.tile(x_center, (8, 1)) + x_unit_circle
 
-    center = geom.add_point(*xc[0], lc)
+    center = geom.add_point(*x_center, lc)
 
     circle_points = []
     for xyz in x_circle:
         p = geom.add_point(*xyz, lc)
-        cirle_points.append(p)
+        circle_points.append(p)
 
-    breakpoint()
-    # gmsh.model.geo.add_circle_arc(startTag, centerTag, endTag)
-    # all lines should be transfinite
+    circle_arcs = []
+    for i in range(len(circle_points)-1):
+        arc = geom.add_circle_arc(circle_points[i], center, circle_points[i+1])
+        circle_arcs.append(arc)
+    arc = geom.add_circle_arc(circle_points[-1], center, circle_points[0])
+    circle_arcs.append(arc)
+    circle_loop = geom.add_curve_loop(circle_arcs)
+
+    circle_surface = geom.add_plane_surface([circle_loop])
 
     # add the rectangle defined by 8 points
+    dx = np.array([width/2, 0., 0.])
+    dy = np.array([0., height/2, 0.])
+    x_rectangle = np.stack([
+        x_center + dx,
+        x_center + dx + dy,
+        x_center + dy,
+        x_center - dx + dy,
+        x_center - dx,
+        x_center - dx - dy,
+        x_center - dy,
+        x_center + dx - dy,
+        ])
 
-    # add diagonal lines between points of the rectangle and the circle
+    rectangle_points = []
+    for xyz in x_rectangle:
+        p = geom.add_point(*xyz, lc)
+        rectangle_points.append(p)
 
-    # set diagonal lines as transfinite
+    # draw rectangle lines
+    rectangle_lines = []
+    for i in range(len(rectangle_points)-1):
+        line = geom.add_line(rectangle_points[i], rectangle_points[i+1])
+        rectangle_lines.append(line)
+    line = geom.add_line(rectangle_points[-1], rectangle_points[0])
+    rectangle_lines.append(line)
 
-    # add the 8 line loops (surfaces between circle and rectangle)
-    # add surfaces accordingly
+    # connect rectangle points and circle points from outer to inner
+    conn = []
+    for i in range(len(circle_points)):
+        l = geom.add_line(rectangle_points[i], circle_points[i])
+        conn.append(l)
 
-    # set surfaces as transfinite (requires points of the surfaces)
+    # add curve loops defining surfaces of the matrix
+    mat_loops = []
+    for i in range(len(circle_points)-1):
+        cloop = geom.add_curve_loop([rectangle_lines[i], conn[i+1], -circle_arcs[i], -conn[i]])
+        mat_loops.append(cloop)
+    cloop = geom.add_curve_loop([rectangle_lines[-1], conn[0], -circle_arcs[-1], -conn[-1]])
+    mat_loops.append(cloop)
 
-    # set physical groups for the matrix and inclusion
+    matrix = []
+    for curve_loop in mat_loops:
+        mat_surface = geom.add_plane_surface([curve_loop])
+        matrix.append(mat_surface)
+
+    if num_cells_per_edge is not None:
+        if not num_cells_per_edge % 2 == 0:
+            raise ValueError("Number of cells per edge must be even for transfinite mesh. Sorry!")
+
+        N = int(num_cells_per_edge) // 2  # num_cells_per_segment
+
+        for line in circle_arcs:
+            geom.mesh.set_transfinite_curve(line, N+1)
+        for line in rectangle_lines:
+            geom.mesh.set_transfinite_curve(line, N+1)
+        # diagonal connections
+        for line in conn[0::2]:
+            geom.mesh.set_transfinite_curve(line, N+3, meshType='Progression', coef=1.0)
+        # horizontal or vertical connections
+        for line in conn[1::2]:
+            geom.mesh.set_transfinite_curve(line, N+3, meshType='Progression', coef=0.9)
+
+        # transfinite surfaces (circle and matrix)
+        # geom.mesh.set_transfinite_surface(tag, arrangement='Left', cornerTags=[])
+        geom.mesh.set_transfinite_surface(circle_surface, arrangement="AlternateLeft", cornerTags=circle_points[0::2])
+        for surface in matrix[0::2]:
+            geom.mesh.set_transfinite_surface(surface, arrangement="Right")
+        for surface in matrix[1::2]:
+            geom.mesh.set_transfinite_surface(surface, arrangement="Left")
+
+    geom.synchronize()
+    geom.removeAllDuplicates()
+
+    # physical groups
+    gmsh.model.add_physical_group(2, matrix, name="matrix")
+    gmsh.model.add_physical_group(2, [circle_surface], name="inclusion")
+
+    grid = _generate_and_read_grid(2)
+    if out_file is not None:
+        _write(out_file, grid, "triangle", prune_z=True)
+
+    return grid
--- multi/test/test_create_rce_grid_01.py
+++ multi/test/test_create_rce_grid_01.py
@@ -1,1 +1,8 @@
+import meshio
+from IPython import embed
 from multi.preprocessing import create_rce_grid_01
+
+
+if __name__ == "__main__":
+    grid = create_rce_grid_01(0., 1., 0., 1., num_cells_per_edge=2)
+    # TODO assert
