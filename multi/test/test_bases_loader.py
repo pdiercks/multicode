a454e9904cde60f3204ff7a67320defc7bc4731f
Philipp Diercks <philipp.diercks@bam.de>
2022-09-28 09:55:10 +0200

    [test] continue test for basis loader

--- multi/test/test_bases_loader.py
+++ multi/test/test_bases_loader.py
@@ -5,7 +5,59 @@ from multi.basis_loader import BasesLoader
 from multi.domain import StructuredGrid
 
 
-if __name__ == "__main__":
+"""test case: block.msh
+cell indices of the mesh
+
+    258
+    147
+    036
+
+• 3x3 quad mesh
+• load basis for each of the nine cells --> need 9 npz files
+• print out bases config?
+• assert number of modes
+• assert shape of each basis
+"""
+DATA = pathlib.Path(__file__).parent / "data"
+
+
+def prepare_test_data(n, modes, cell_index):
+    phi = np.ones((8, n))
+    bottom = np.ones((modes[0], n)) * 2
+    right = np.ones((modes[1], n)) * 3
+    top = np.ones((modes[2], n)) * 4
+    left = np.ones((modes[3], n)) * 5
+    np.savez(DATA / f"basis_{cell_index:03}.npz", phi=phi, b=bottom, r=right, t=top, l=left)
+
+
+def test_bases_loader_read_bases():
+    """
+    258
+    147
+    036
+    """
+    V_dim = 10
+    prepare_test_data(V_dim, [9, 99, 99, 9], 0)
+    prepare_test_data(V_dim, [1, 99, 1, 1], 1)
+    prepare_test_data(V_dim, [99, 99, 2, 2], 2)
+    prerare_test_data(V_dim, [3, 3, 99, 3], 3)
+    prepare_test_data(V_dim, [4, 4, 4, 4], 4)
+    prepare_test_data(V_dim, [99, 5, 5, 5], 5)
+    prepare_test_data(V_dim, [6, 6, 99, 99], 6)
+    prepare_test_data(V_dim, [7, 7, 7, 99], 7)
+    prepare_test_data(V_dim, [99,8,8,9999], 8)
+
+    expected_num_modes = np.array([
+        [9, 3, 1, 9],
+        [1, 4, 1, 1],
+        [1, 5, 2, 2],
+        [3, 3, 4, 3],
+        [4, 4, 4, 4],
+        [4, 5, 5, 5],
+        [6, 6, 7, 3],
+        [7, 7, 7, 4],
+        [7, 8, 8, 5]])
+
     msh_file = pathlib.Path(__file__).parent / "data/block.msh"
     mesh = meshio.read(msh_file.as_posix())
 
@@ -30,3 +82,17 @@ if __name__ == "__main__":
 
     directory = pathlib.Path("./data/")
     loader = BasesLoader(directory, grid)
+    bases, modes = loader.read_bases()
+
+    assert len(bases) == 9
+    assert np.allclose(expected_num_modes, modes)
+    expected = np.sum(expected_num_modes, axis=0)
+    result = np.array([], dtype=int)
+    for i in range(9):
+        result = np.append(bases[i].shape[0])
+    assert np.allclose(expected, result)
+
+
+
+if __name__ == "__main__":
+    test_bases_loader_read_bases()
