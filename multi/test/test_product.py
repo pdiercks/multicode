ec1415c907f7a357ad32e11eec29d2f511227f07
pdiercks <philipp.diercks@bam.de>
2022-10-02 16:46:39 +0200

    [product] migrate to fenicsx

--- multi/test/test_product.py
+++ multi/test/test_product.py
@@ -1,26 +1,32 @@
 """test inner product"""
 
 import ufl
-import dolfin as df
-from multi import InnerProduct
+import dolfinx
+from mpi4py import MPI
+from petsc4py import PETSc
+from multi.product import InnerProduct
 
 
 def test():
-    mesh = df.UnitSquareMesh(4, 4)
-    V = df.VectorFunctionSpace(mesh, "CG", 1)
+    domain = dolfinx.mesh.create_unit_interval(MPI.COMM_WORLD, 10)
+    V = dolfinx.fem.FunctionSpace(domain, ("CG", 1))
     names = ("mass", "l2", "h1-semi", "stiffness", "h1")
     for name in names:
         prod = InnerProduct(V, name, bcs=())
         assert isinstance(prod.get_form(), ufl.form.Form)
-        assert isinstance(prod.assemble(), df.cpp.la.Matrix)
+        assert isinstance(prod.assemble_matrix(), PETSc.Mat)
+
+    A = prod.assemble_matrix()
+    assert A.size == (11, 11)
+    assert A[0, 0] > 0.0
 
     prod = InnerProduct(V, "euclidean")
     assert prod.get_form() is None
-    assert prod.assemble() is None
+    assert prod.assemble_matrix() is None
 
     prod = InnerProduct(V, None)
     assert prod.get_form() is None
-    assert prod.assemble() is None
+    assert prod.assemble_matrix() is None
 
 
 if __name__ == "__main__":
