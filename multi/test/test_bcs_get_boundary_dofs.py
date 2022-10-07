d5a65ae986c3823e3dc2bad1a7b3f1910c3a0f76
Philipp Diercks <philipp.diercks@bam.de>
2022-10-07 17:55:07 +0200

    update test

--- multi/test/test_bcs_get_boundary_dofs.py
+++ multi/test/test_bcs_get_boundary_dofs.py
@@ -1,6 +1,6 @@
 import dolfinx
 from mpi4py import MPI
-from multi.bcs import get_boundary_dofs
+from multi.bcs import BoundaryConditions
 
 
 def test():
@@ -12,7 +12,14 @@ def test():
         MPI.COMM_WORLD, n, n, dolfinx.mesh.CellType.quadrilateral
     )
     V = dolfinx.fem.VectorFunctionSpace(domain, ("CG", degree), dim=dim)
-    dofs = get_boundary_dofs(V)
+
+    bc_handler = BoundaryConditions(domain, V)
+    boundary_facets = dolfinx.mesh.exterior_facet_indices(domain.topology)
+    u = dolfinx.fem.Function(V)
+    u.x.set(0.)
+    bc_handler.add_dirichlet_bc(u, boundary_facets, method="topological", entity_dim=1)
+    bcs = bc_handler.bcs
+    dofs = bcs[0].dof_indices()[0]
     assert dofs.size == n * 4 * degree * dim
     # there are (n+1) * 4 - 4 points
     # and if degree == 2: additional n*4 dofs (edges)
