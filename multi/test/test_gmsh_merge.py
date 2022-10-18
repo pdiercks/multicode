363ddaf10fb10ce1d3d8b90d9323f6f3cbbd76f6
pdiercks <philipp.diercks@bam.de>
2022-10-18 16:26:20 +0200

    [test] add test for merging msh files with gmsh

---
+++ multi/test/test_gmsh_merge.py
@@ -0,0 +1,65 @@
+import dolfinx
+from mpi4py import MPI
+import gmsh
+import tempfile
+import pathlib
+import meshio
+from multi.preprocessing import create_mesh,  create_rce_grid_01
+
+
+def merge(mshfiles, output):
+    """loads several .msh files and merges them"""
+    gmsh.initialize()
+    gmsh.clear()
+    gmsh.model.add("merged")
+
+    for msh_file in mshfiles:
+        gmsh.merge(msh_file)
+
+    gmsh.model.geo.remove_all_duplicates()
+    gmsh.model.geo.synchronize()
+    gmsh.model.mesh.remove_duplicate_nodes()
+    gmsh.model.mesh.remove_duplicate_elements()
+
+    gmsh.model.mesh.generate(2)
+
+    gmsh.write(output)
+    gmsh.finalize()
+
+
+if __name__ == "__main__":
+    coord = [
+            [0., 1., 0., 1.],
+            [1., 2., 0., 1.]
+            ]
+    num_cells = 2
+    to_be_merged = []
+    tfiles = []
+    tfiles.append(tempfile.NamedTemporaryFile(suffix=".msh"))
+    tfiles.append(tempfile.NamedTemporaryFile(suffix=".msh"))
+    for i in range(num_cells):
+        tmp = tfiles[i]
+        xmin, xmax, ymin, ymax = coord[i]
+        create_rce_grid_01(xmin, xmax, ymin, ymax, 
+                num_cells_per_edge=10, facets=False, out_file=tmp.name)
+        to_be_merged.append(tmp.name)
+    merge(to_be_merged, "final_mesh.msh")
+    # cannot be read with dolfin because of same tags for different surfaces etc.
+
+    for tf in tfiles:
+        tf.close()
+
+    # write to xdmf using meshio, then dolfinx.io.XDMFFile ...
+    in_mesh = meshio.read("final_mesh.msh")
+
+    triangle_mesh = create_mesh(in_mesh, "triangle", prune_z=True)
+    meshio.write("final_mesh.xdmf", triangle_mesh)
+
+    with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "final_mesh.xdmf", "r") as xdmf:
+        mesh = xdmf.read_mesh(name="Grid")
+        ct = xdmf.read_meshtags(mesh, name="Grid")
+
+    assert ct.find(0).size == 0
+    assert ct.find(1).size > 0
+    assert ct.find(2).size > 0
+    assert ct.find(3).size == 0
