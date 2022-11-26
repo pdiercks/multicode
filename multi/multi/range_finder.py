68d0b5c6a395dc6e4250326cb8573757677424c1
Philipp Diercks <philipp.diercks@gmail.com>
2022-11-26 01:50:21 +0100

    [range_finder] add draft for target interface

--- multi/multi/range_finder.py
+++ multi/multi/range_finder.py
@@ -1,14 +1,19 @@
+import dolfinx
 import numpy as np
+from petsc4py.PETSc import ScalarType
 
 from pymor.algorithms.gram_schmidt import gram_schmidt
 from pymor.core.logger import getLogger
 from pymor.operators.interface import Operator
 from pymor.tools.timing import Timer
+from pymor.reductors.basic import extend_basis
 
 from scipy.linalg import eigh
 from scipy.sparse.linalg import eigsh, LinearOperator
 from scipy.special import erfinv
 
+from multi.product import InnerProduct
+from multi.dofmap import QuadrilateralDofLayout
 from multi.sampling import correlation_function
 
 
@@ -303,3 +308,222 @@ def adaptive_rrf(
         logger.info(f"{maxnorm=}")
 
     return U
+
+
+# modified version of pymor.algorithms.rand_la.adaptive_rrf
+def adaptive_edge_rrf(
+    transfer_problem,
+    random_state,
+    distribution,
+    active_edges,
+    source_product=None,
+    range_product=None,
+    error_tol=1e-4,
+    failure_tolerance=1e-15,
+    num_testvecs=20,
+    lambda_min=None,
+    **sampling_options,
+):
+    r"""Adaptive randomized range approximation of `A`.
+    This is an implementation of Algorithm 1 in [BS18]_.
+
+    Given the |Operator| `A`, the return value of this method is the |VectorArray|
+    `B` with the property
+
+    .. math::
+        \Vert A - P_{span(B)} A \Vert \leq tol
+
+    with a failure probability smaller than `failure_tolerance`, where the norm denotes the
+    operator norm. The inner product of the range of `A` is given by `range_product` and
+    the inner product of the source of `A` is given by `source_product`.
+
+    NOTE
+    ----
+    Instead of a transfer operator A, a transfer problem is used.
+    (see multi.problem.TransferProblem)
+    The image Av = A.apply(v) is equivalent to the restriction
+    of the full solution to the target domain Ω_in, i.e.
+        U = transfer_problem.solve(v)
+
+
+    Parameters
+    ----------
+    transfer_problem
+        The transfer problem associated with a (transfer) |Operator| A.
+    random_state
+        The random state to generate samples.
+    distribution
+        The distribution to generate samples from.
+    active_edges
+        A list of edges of the target subdomain.
+    source_product
+        Inner product |Operator| of the source of A.
+    range_product
+        A str specifying which inner product to use.
+    error_tol
+        Error tolerance for the algorithm.
+    failure_tolerance
+        Maximum failure probability.
+    num_testvecs
+        Number of test vectors.
+    lambda_min
+        The smallest eigenvalue of source_product.
+        If `None`, the smallest eigenvalue is computed using scipy.
+    sampling_options
+        Optional keyword arguments for the generation of
+        random samples (training data).
+        see `_create_random_values`.
+
+    Returns
+    -------
+     U
+        |VectorArray| which contains the (non-orthonormal) solutions, whose
+        span approximates the range of A.
+    """
+
+    logger = getLogger("multi.range_finder.adaptive_edge_rrf")
+    tp = transfer_problem
+
+    assert source_product is None or isinstance(source_product, Operator)
+    # TODO remove arg range_product or let this be a list
+    assert range_product is None or isinstance(range_product, Operator)
+
+    if source_product is None:
+        lambda_min = 1
+    elif lambda_min is None:
+
+        def mv(v):
+            return source_product.apply(source_product.source.from_numpy(v)).to_numpy()
+
+        def mvinv(v):
+            return source_product.apply_inverse(
+                source_product.range.from_numpy(v)
+            ).to_numpy()
+
+        L = LinearOperator(
+            (source_product.source.dim, source_product.range.dim), matvec=mv
+        )
+        Linv = LinearOperator(
+            (source_product.range.dim, source_product.source.dim), matvec=mvinv
+        )
+        lambda_min = eigsh(
+            L, sigma=0, which="LM", return_eigenvectors=False, k=1, OPinv=Linv
+        )[0]
+
+
+    if distribution == "multivariate_normal":
+        raise NotImplementedError
+
+    # ### test set
+    R = tp.generate_random_boundary_data(
+            count=num_testvecs, distribution=distribution,
+            random_state=random_state, **sampling_options
+            )
+    M = tp.solve(R)
+
+    # TODO Now it would be great if TransferProblem would have a real subdomain
+    # problem with subdomain_problem.edge_spaces etc. as attributes
+
+    dof_layout = QuadrilateralDofLayout()
+    edge_index_map = dof_layout.local_edge_index_map
+
+    # ### initialize data structures
+    test_sets = {}
+    range_spaces = {}
+    range_products = {}
+    pod_bases = {}
+    maxnorm = np.array([], dtype=float)
+    edges = np.array([], dtype=str)
+    coarse_basis = {}
+    # the dofs for vertices on the boundary of the edge
+    edge_boundary_dofs = {}
+
+    for i in range(dof_layout.num_entities[1]):
+        edge = edge_index_map[i]
+        edges = np.append(edges, edge)
+
+        edge_mesh = tp.subproblem.edges[edge]
+        edge_space = tp.subproblem.edge_spaces[edge]
+        range_spaces[edge] = FenicsxVectorSpace(edge_space)
+
+        # ### create dirichletbc for range product
+        facet_dim = edge_mesh.topology.dim - 1
+        vertices = dolfinx.mesh.locate_entities_boundary(
+                edge_mesh, facet_dim, lambda x: np.full(x[0].shape, True, dtype=bool)
+                )
+        _dofs_ = dolfinx.fem.locate_dofs_topological(edge_space, facet_dim, vertices)
+        range_bc = dolfinx.fem.dirichletbc(np.array((0, ) * (facet_dim + 1), dtype=ScalarType), _dofs_)
+        edge_boundary_dofs[edge] = range_bc.dof_indices()[0]
+
+        # ### range product
+        inner_product = InnerProduct(edge_space, range_product, bcs=(range_bc, ))
+        range_product = FenicsxMatrixOperator(inner_product.assemble_matrix(), edge_space, edge_space)
+        range_products[edge] = range_product
+
+        # ### compute coarse scale edge basis
+        nodes = dolfinx.mesh.compute_midpoints(edge_mesh, facet_dim, vertices)
+        nodes = np.around(nodes, decimals=3)
+
+        if edge in ("bottom", "top"):
+            component = 0
+        elif edge in ("left", "right"):
+            component = 1
+    
+        line_element = NumpyLine(nodes[:, component])
+        shape_funcs = line.interpolate(edge_space, component)
+        N = range_spaces[edge].from_numpy(shape_funcs)
+        coarse_basis[edge] = N
+
+        # ### edge test sets
+        dofs = tp.subproblem.V_to_L[edge]
+        test_sets[edge] = range_spaces[edge].from_numpy(M.dofs(dofs))
+
+        # ### pod bases
+        pod_bases[edge] = range_spaces[edge].empty()
+
+        # ### initialize maxnorm
+        if edge in active_edges:
+            maxnorm = np.append(maxnorm, np.inf)
+        else:
+            maxnorm = np.append(maxnorm, 0.0)
+
+    # NOTE tp.source is the full space, while the source product is of lower dimension
+    num_source_dofs = len(tp._bc_dofs_gamma_out)
+    testfail = np.array([failure_tolerance / min(num_source_dofs, Srange.dim) for Srange in range_spaces.values()])
+    testlimit = (
+        np.sqrt(2.0 * lambda_min) * erfinv(testfail ** (1.0 / num_testvecs)) * error_tol
+    )
+
+    logger.info(f"{lambda_min=}")
+    logger.info(f"{testlimit=}")
+
+
+    while np.any(maxnorm > testlimit):
+        basis_length = [len(rb) for rb in pod_bases.values()]
+
+        v = tp.generate_random_boundary_data(1, distribution, random_state, **kwargs)
+        U = tp.solve(v)
+
+        target_edges = edges[maxnorm > testlimit]
+        for edge in target_edges:
+            B = pod_bases[edge]
+            edge_space = range_spaces[edge]
+            # restrict the training sample to the edge
+            Udofs = edge_space.from_numpy(U.dofs(tp.subproblem.V_to_L[edge]))
+            coarse_values = Udofs.dofs(edge_boundary_dofs[edge])
+            U_fine = Udofs - coarse_basis[edge].lincomb(coarse_values)
+
+            # extend pod basis
+            extend_basis(U_fine, rb, product=None, method="pod")
+
+            # orthonormalize test set wrt pod basis
+            M = test_set[edge]
+            # FIXME there should be 4 range_products
+            M -= B.lincomb(B.inner(M, range_product).T)
+
+            norm = M.norm(range_product)
+            maxnorm[edge_index_map[edge]] = np.max(norm)
+
+        logger.info(f"{maxnorm=}")
+
+    return pod_bases
