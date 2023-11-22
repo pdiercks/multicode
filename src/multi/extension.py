ba615858dbee863bf4b601dfbade057596fc886f
Philipp Diercks <philipp.diercks@bam.de>
2023-11-22 15:17:09 +0100

    remove restrict function

--- src/multi/extension.py
+++ src/multi/extension.py
@@ -1,5 +1,5 @@
-import dolfinx
-
+from dolfinx import fem
+from dolfinx import mesh
 
 def extend(problem, boundary_data, petsc_options={}):
     """extend the `boundary_data` into the domain of the `problem`
@@ -23,9 +23,9 @@ def extend(problem, boundary_data, petsc_options={}):
 
     # add dummy bc on whole boundary
     # to zero out rows and columns of matrix A
-    zero_fun = dolfinx.fem.Function(V)
+    zero_fun = fem.Function(V)
     zero_fun.vector.zeroEntries()
-    boundary_facets = dolfinx.mesh.exterior_facet_indices(domain.topology)
+    boundary_facets = mesh.exterior_facet_indices(domain.topology)
     problem.add_dirichlet_bc(
         zero_fun, boundary_facets, method="topological", entity_dim=fdim
     )
@@ -42,7 +42,7 @@ def extend(problem, boundary_data, petsc_options={}):
     # define all extensions that should be computed
     assert all(
         [
-            isinstance(bc, dolfinx.fem.DirichletBCMetaClass)
+            isinstance(bc, fem.DirichletBC)
             for bcs in boundary_data
             for bc in bcs
         ]
@@ -51,7 +51,7 @@ def extend(problem, boundary_data, petsc_options={}):
     # initialize rhs vector
     rhs = problem.b
     # initialize solution
-    u = dolfinx.fem.Function(problem.V)
+    u = fem.Function(problem.V)
 
     extensions = []
     for bcs in boundary_data:
@@ -66,46 +66,3 @@ def extend(problem, boundary_data, petsc_options={}):
         extensions.append(u.vector.copy())
 
     return extensions
-
-
-# FIXME dim or boundary, but not both are required?
-# TODO can use interpolation between different meshes now
-# maybe this is not needed anymore
-def restrict(function, marker, dim, boundary=False):
-    """restrict the function to some part of the domain
-
-    Parameters
-    ----------
-    function : dolfinx.fem.Function
-        The function to be evaluated at the boundary.
-    marker : callable
-        A function that defines the subdomain geometrically.
-        `dolfinx.mesh.locate_entities` is used.
-    dim : int
-        Topological dimension of the entities.
-    boundary : optional, bool
-        If True, use `dolfinx.mesh.locate_entities_boundary` and
-        locate entities on the boundary only.
-
-    Returns
-    -------
-    function values restricted to some part of the domain
-    """
-
-    V = function.function_space
-    domain = V.mesh
-    tdim = domain.topology.dim
-    fdim = tdim - 1
-    assert dim in (fdim, tdim)
-
-    if boundary:
-        entities = dolfinx.mesh.locate_entities_boundary(domain, fdim, marker)
-    else:
-        entities = dolfinx.mesh.locate_entities(domain, tdim, marker)
-
-    dofs = dolfinx.fem.locate_dofs_topological(V, dim, entities)
-    dummy = dolfinx.fem.Function(V)
-    dummy.x.set(0.0)
-    bc = dolfinx.fem.dirichletbc(dummy, dofs)
-    dof_indices = bc.dof_indices()[0]
-    return function.vector.array[dof_indices]
