9d2964bd95570cc8d8105dbea261b721c5ca4cae
Philipp Diercks <philipp.diercks@bam.de>
2023-12-14 17:12:12 +0100

    transfer_operator: more tests

--- src/multi/transfer_operator.py
+++ src/multi/transfer_operator.py
@@ -12,14 +12,15 @@ from pymor.bindings.fenicsx import FenicsxVectorSpace, FenicsxMatrixOperator
 from multi.domain import Domain
 from multi.bcs import get_boundary_dofs
 from multi.boundary import within_range
-# from multi.misc import x_dofs_vectorspace
-from multi.product import InnerProduct
 from multi.materials import LinearElasticMaterial
+from multi.product import InnerProduct
+from multi.projection import orthogonal_part
 from multi.problems import LinearElasticityProblem
 from multi.solver import build_nullspace
 
 
-def transfer_operator_subdomains_2d(A, dirichlet_dofs, target_dofs):
+def transfer_operator_subdomains_2d(A, dirichlet_dofs, target_dofs, projection_matrix=None):
+    # FIXME what format (csr, csc) should A have for efficient slicing?
 
     # dirichlet dofs associated with Γ_out
     num_dofs = A.shape[0]
@@ -47,49 +48,16 @@ def transfer_operator_subdomains_2d(A, dirichlet_dofs, target_dofs):
     end = time.time()
     print(f"applied operator to rhs in {end-start}", flush=True)
 
-    # TODO projection | remove kernel
-    # TODO compute projection matrix P as R inv( R^T R ) R^T, with R holding rigid body modes as column vectors
-    # if ar:
-    #     ar = average_remover(transfer_operator.shape[0])
-    #     transfer_operator = ar.dot(transfer_operator)
+    if projection_matrix is not None:
+        # remove kernel
+        assert transfer_operator.shape[0] == projection_matrix.shape[0]
+        P = np.eye(transfer_operator.shape[0]) - projection_matrix
+        transfer_operator = P.dot(transfer_operator)
 
     return NumpyMatrixOperator(transfer_operator)
 
 
-def test_matrix_of_projection():
-    # assemble matrix of rigid body modes --> R
-    # orthonormalize
-    # compute R inv( R^T R ) R^T
-    domain = mesh.create_unit_square(MPI.COMM_WORLD, 2, 2, mesh.CellType.quadrilateral)
-    fe_deg = 1
-    fe = element("P", domain.basix_cell(), fe_deg, shape=(2,))
-    V = fem.functionspace(domain, fe)
-
-    source = FenicsxVectorSpace(V)
-    product = InnerProduct(V, product="h1")
-    product_mat = product.assemble_matrix()
-    product = FenicsxMatrixOperator(product_mat, V, V)
-    ns = build_nullspace(source, product=product)
-
-    u = fem.Function(V)
-    gamma = 0.2 # simple shear factor
-    u.interpolate(lambda x: [0.5 + x[1] * gamma, 0.3 + x[0] * 0.]) # type: ignore
-    U = source.make_array([u.vector]) # type: ignore
-
-    # projection
-    coeff = U.inner(ns, product)
-    U_proj = ns.lincomb(coeff)
-
-    R = ns.to_numpy()
-    A = R.T
-    P = np.dot(A, np.dot(np.linalg.inv(np.dot(A.T, A)), A.T))
-    result = np.dot(P, U.to_numpy().flatten())
-    assert np.allclose(result, U_proj.to_numpy().flatten())
-
-
 if __name__ == "__main__":
-    test_matrix_of_projection()
-
     # create oversampling domain 3x3 coarse cells
     # each coarse cell is discretized with 2x2 quads
     domain = mesh.create_unit_square(MPI.COMM_WORLD, 6, 6, mesh.CellType.quadrilateral)
@@ -121,23 +89,90 @@ if __name__ == "__main__":
     bc_boundary = fem.dirichletbc(zero, boundary_dofs, V)
     bc_target = fem.dirichletbc(zero, range_dofs, V)
 
+    # build projection matrix in the range space!
+    # P = R inv( R^T R ) R^T, with R holding rigid body modes as column vectors
+    # TODO build submesh ... then space on target subdomain
+    tdim = domain.topology.dim
+    cells_in = mesh.locate_entities(domain, tdim, target_subdomain)
+    omega_in, _, _, _= mesh.create_submesh(domain, tdim, cells_in)
+    W = fem.functionspace(omega_in, fe)
+    rangespace = FenicsxVectorSpace(W)
+
+    # select inner product
+    # inner_product = "eulidean"
+    inner_product = "h1"
+    # inner_product = "mass"
+
+    product = None
+    M = np.eye(rangespace.dim)
+    if not inner_product == "eulidean":
+        product = InnerProduct(W, product=inner_product)
+        product_mat = product.assemble_matrix()
+        product = FenicsxMatrixOperator(product_mat, W, W)
+        M = product_mat[:, :]
+
+    basis = build_nullspace(rangespace, product=product)
+    R = basis.to_numpy().T
+
+    right = np.dot(R.T, M)
+    middle = np.linalg.inv(np.dot(R.T, np.dot(M, R)))
+    left = R
+    P = np.dot(left, np.dot(middle, right))
+
+    random_values = rangespace.random(1)
+    coeff = np.array([1.3, 4.9, 0.6])
+    # coeff.shape[-1] has to agree with len(basis)
+    U = basis.lincomb(coeff) + random_values
+
+    # ### compute orthogonal part
+    # 1. via matrix
+    r1 = (np.eye(P.shape[0]) - P).dot(U.to_numpy().flatten())
+
+    # 2. solving linear system
+    G = basis.gramian(product=product)
+    rhs = basis.inner(U, product=product)
+    v = np.linalg.solve(G, rhs)
+    U_proj = basis.lincomb(v.T)
+    r2 = U - U_proj
+    # r1 and r2 are equal
+    # but they do not match random values
+    # the projection will try to find closest match
+    # but will make an error since its not possible to represent U in basis
+    error = r1 - r2.to_numpy().flatten()
+    assert np.linalg.norm(error) < 1e-12
+
     # build transfer operator
     boundary_dofs_full = bc_boundary._cpp_object.dof_indices()[0]
     range_dofs_full = bc_target._cpp_object.dof_indices()[0]
-    T = transfer_operator_subdomains_2d(A, boundary_dofs_full, range_dofs_full)
+    T = transfer_operator_subdomains_2d(A, boundary_dofs_full, range_dofs_full, projection_matrix=None)
     # compare T.apply to A.apply_inverse restricted to target subdomain
     U = T.source.random(1)
     TU = T.apply(U)
+
+    Tproj = transfer_operator_subdomains_2d(A, boundary_dofs_full, range_dofs_full, projection_matrix=P)
+    TpU = Tproj.apply(U)
 
     # reference solution
     g = fem.Function(V)
     g.vector.array[boundary_dofs_full] = U.to_numpy().flatten()
     bc = fem.dirichletbc(g, boundary_dofs)
-
     problem.clear_bcs()
     problem.add_dirichlet_bc(bc)
     problem.setup_solver()
     u = problem.solve()
     u_in = u.vector.array[range_dofs_full]
+
+    # comparison without removing the kernel
     error = u_in - TU.to_numpy().flatten()
+    assert np.linalg.norm(error) < 1e-9
+
+    # comparison with removing the kernel
+    u_in_orth = (np.eye(P.shape[0]) - P).dot(u_in)
+    error = u_in_orth - TpU.to_numpy().flatten()
+    assert np.linalg.norm(error) < 1e-9
+
+    # test against alternative orthogonal part computation
+    UIN = rangespace.from_numpy([u_in])
+    U_orth = orthogonal_part(basis, UIN, product, orth=False)
+    error = U_orth.to_numpy().flatten() - TpU.to_numpy().flatten()
     assert np.linalg.norm(error) < 1e-9
