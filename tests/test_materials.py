effa5465976bfb9f8dc4b675cca26704b4a0c6d2
Philipp Diercks <philipp.diercks@bam.de>
2023-12-21 16:37:52 +0100

    cov materials 100

--- tests/test_materials.py
+++ tests/test_materials.py
@@ -14,12 +14,14 @@ def test(gdim):
 
     mat = LinearElasticMaterial(gdim, E=E, NU=NU)
     if gdim == 1:
+        cell_type = basix.CellType.interval
         domain = mesh.create_unit_interval(MPI.COMM_WORLD, 1)
     elif gdim == 2:
+        cell_type = basix.CellType.quadrilateral
         domain = mesh.create_unit_square(MPI.COMM_WORLD, 1, 1, mesh.CellType.quadrilateral)
-        mat_edz = LinearElasticMaterial(gdim, E=E, NU=NU, plane_stress=False)
-        mat_esz = LinearElasticMaterial(gdim, E=E, NU=NU, plane_stress=True)
+        mat = LinearElasticMaterial(gdim, E=E, NU=NU, plane_stress=False)
     else:
+        cell_type = basix.CellType.hexahedron
         domain = mesh.create_unit_cube(MPI.COMM_WORLD, 1, 1, 1, mesh.CellType.hexahedron)
 
     fe = element("P", domain.basix_cell(), 1, shape=(gdim,))
@@ -28,18 +30,42 @@ def test(gdim):
     map_c = domain.topology.index_map(domain.topology.dim)
     num_cells = map_c.size_local + map_c.num_ghosts
     cells = np.arange(0, num_cells, dtype=np.int32)
+    ipoint, _ = basix.make_quadrature(cell_type, 1)
+    ix = ipoint[0, 0]
 
-    if gdim == 1:
-        u = fem.Function(V)
-        magic_number = 12.4
-        u.interpolate(lambda x: (x[0] ** 2 + magic_number * x[0],))
+    u = fem.Function(V)
+    alpha = 12.4
 
-        points, _ = basix.make_quadrature(basix.CellType.interval, 1)
-        strain_expr = fem.Expression(mat.eps(u), points)
-        strain_vals = strain_expr.eval(domain, cells)
-        assert strain_vals.size == 9
-        assert np.isclose(np.sum(strain_vals), 1. + magic_number)
+    def u_data_factory(gdim):
+
+        def u(x):
+            zero = np.zeros_like(x[0])
+            ux = x[0] ** 2 + alpha * x[0]
+            uy = x[0] + x[1]
+            uz = zero
+            if gdim == 1:
+                return (ux, )
+            elif gdim == 2:
+                return (ux, uy)
+            elif gdim == 3:
+                return (ux, uy, uz)
+
+        return u
+
+    u.interpolate(u_data_factory(gdim))
+
+    def strain_sum(gdim):
+        # define analytic solution for each gdim
+        if gdim == 1:
+            return 2. * ix + alpha
+        elif gdim > 1:
+            return 2. * ix + alpha + 2.
+
+    strain_expr = fem.Expression(mat.eps(u), ipoint)
+    strain_vals = strain_expr.eval(domain, cells)
+    assert strain_vals.size == 9
+    assert np.isclose(np.sum(strain_vals), strain_sum(gdim))
 
 
 if __name__ == "__main__":
-    test(1)
+    test(3)
