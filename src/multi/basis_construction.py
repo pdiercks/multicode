8ad48dd19d13373d2fe4205c84f8a94f7ab7ff41
Philipp Diercks <philipp.diercks@bam.de>
2023-12-21 17:45:15 +0100

    cov basis constrction 100

--- src/multi/basis_construction.py
+++ src/multi/basis_construction.py
@@ -1,14 +1,6 @@
-import dolfinx
-from dolfinx.io import gmshio
-from mpi4py import MPI
-import numpy as np
-import yaml
-
-from multi.problems import LinearElasticityProblem
-from multi.domain import RectangularDomain
+from dolfinx.fem import Function
 from multi.bcs import BoundaryDataFactory
 from multi.extension import extend
-
 from multi.shapes import NumpyQuad
 
 
@@ -21,7 +13,7 @@ def compute_phi(problem, nodes):
     data_factory = BoundaryDataFactory(problem.domain.grid, V)
 
     boundary_data = []
-    g = dolfinx.fem.Function(V)
+    g = Function(V)
     for shape in shape_functions:
         g.x.array[:] = shape
         boundary_data.append([data_factory.create_bc(g.copy())])
@@ -33,50 +25,3 @@ def compute_phi(problem, nodes):
     }
     phi = extend(problem, boundary_data, petsc_options=petsc_options)
     return phi
-
-
-def compute_coarse_scale_basis(rce_grid, material, degree, out_file):
-    """compute the coarse scale basis (extension of bilinear shape functions)
-
-    NOTE
-    ----
-    method to be used within python action of a dodoFile
-
-    Parameters
-    ----------
-    rce_grid : filepath
-        The partition of the subdomain.
-    material : filepath
-        The material parameters (.yaml).
-    degree : int
-        Degree of the VectorFunctionSpace
-    """
-    domain, cell_marker, facet_marker = gmshio.read_from_msh(
-        rce_grid, MPI.COMM_SELF, gdim=2
-    )
-    omega = RectangularDomain(domain, cell_marker, facet_marker, index=0)
-    V = dolfinx.fem.VectorFunctionSpace(domain, ("Lagrange", degree))
-
-    # FIXME define nodes via multi.dofmap.QuadrilateralDofLayout ?
-    xmin = omega.xmin
-    xmax = omega.xmax
-    nodes = np.array([
-        [xmin[0], xmin[1], 0.],
-        [xmax[0], xmin[1], 0.],
-        [xmin[0], xmax[1], 0.],
-        [xmax[0], xmax[1], 0.]
-        ])
-
-    with material.open("r") as f:
-        mat = yaml.safe_load(f)
-
-    E = mat["Material parameters"]["E"]["value"]
-    NU = mat["Material parameters"]["NU"]["value"]
-    plane_stress = mat["Constraints"]["plane_stress"]
-    problem = LinearElasticityProblem(omega, V, E=E, NU=NU, plane_stress=plane_stress)
-    basis_vectors = compute_phi(problem, nodes)
-    out = []
-    for vec in basis_vectors:
-        out.append(vec.array)
-
-    np.savez(out_file, phi=out)
