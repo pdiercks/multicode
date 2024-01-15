6b8d9057f58a8e287034ef6cc23cc9cc063c1ca4
Philipp Diercks <philipp.diercks@bam.de>
2024-01-15 14:49:34 +0100

    debug: fix plot_modes

--- src/multi/debug.py
+++ src/multi/debug.py
@@ -5,14 +5,14 @@ import numpy as np
 def plot_modes(edge_space, edge, modes, component, mask):
     """for hacking"""
     L = edge_space
-    x_dofs = L.sub(0).collapse().tabulate_dof_coordinates()
+    x_dofs = L.tabulate_dof_coordinates()
 
     if component in ("x", 0):
         modes = modes[:, ::2]
     elif component in ("y", 1):
         modes = modes[:, 1::2]
 
-    assert edge in ("b", "r", "t", "l")
+    assert edge in ("b", "bottom", "r", "right", "t", "top", "l", "left")
     xx = x_dofs[:, 0] # b or t
     if edge in ("r", "l"):
         xx = x_dofs[:, 1]
