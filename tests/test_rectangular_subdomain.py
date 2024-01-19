90a28693ee5705cdde3a3cfd3855599f86318403
Philipp Diercks <philipp.diercks@bam.de>
2024-01-19 14:09:10 +0100

    RectangularDomain: add method to create facet tags

---
+++ tests/test_rectangular_subdomain.py
@@ -0,0 +1,55 @@
+import tempfile
+import pytest
+import numpy as np
+from mpi4py import MPI
+from dolfinx import mesh
+from dolfinx.io import gmshio
+from multi.preprocessing import create_rectangle
+from multi.domain import RectangularSubdomain
+
+
+def get_unit_square_mesh(nx=8, ny=8):
+    with tempfile.NamedTemporaryFile(suffix=".msh") as tf:
+        create_rectangle(0., 1., 0., 1., num_cells=(nx, ny), recombine=True, out_file=tf.name)
+        domain, _, _ = gmshio.read_from_msh(tf.name, MPI.COMM_WORLD, gdim=2)
+    return domain
+
+
+def test():
+    rectangle = RectangularSubdomain(17, get_unit_square_mesh(10, 10))
+    rectangle.create_coarse_grid(1)
+    rectangle.create_boundary_grids()
+    assert len(rectangle.fine_edge_grid.keys()) == 4
+    assert len(rectangle.coarse_edge_grid.keys()) == 4
+    assert isinstance(rectangle.fine_edge_grid["bottom"], mesh.Mesh)
+    assert isinstance(rectangle.fine_edge_grid["top"], mesh.Mesh)
+    assert isinstance(rectangle.fine_edge_grid["right"], mesh.Mesh)
+    assert isinstance(rectangle.fine_edge_grid["left"], mesh.Mesh)
+    assert isinstance(rectangle.coarse_edge_grid["bottom"], mesh.Mesh)
+    assert isinstance(rectangle.coarse_edge_grid["top"], mesh.Mesh)
+    assert isinstance(rectangle.coarse_edge_grid["right"], mesh.Mesh)
+    assert isinstance(rectangle.coarse_edge_grid["left"], mesh.Mesh)
+
+    rectangle = RectangularSubdomain(18, get_unit_square_mesh(10, 10))
+    rectangle.create_coarse_grid(2)
+    cgrid = rectangle.coarse_grid
+    assert isinstance(cgrid, mesh.Mesh)
+    assert cgrid.topology.dim == 2
+    num_cells = cgrid.topology.index_map(cgrid.topology.dim).size_local
+    assert num_cells == 4
+
+    bottom = rectangle.str_to_marker("bottom")
+    top = rectangle.str_to_marker("top")
+    left = rectangle.str_to_marker("left")
+    right = rectangle.str_to_marker("right")
+    origin = np.array([[0.], [0.], [0.]])
+    assert bottom(origin)
+    assert left(origin)
+    assert not top(origin)
+    assert not right(origin)
+
+    with pytest.raises(AttributeError):
+        rectangle.create_coarse_grid(1)
+
+    with pytest.raises(ValueError):
+        rectangle.str_to_marker("valueerror")
