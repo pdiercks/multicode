b2156b27dafff572a93f5f64855cefe102bc6b4f
Philipp Diercks <philipp.diercks@bam.de>
2024-01-26 14:37:06 +0100

    refactor plotting context

--- src/multi/plotting_context.py
+++ src/multi/plotting_context.py
@@ -1,6 +1,8 @@
-"""PlottingContext written by Christoph Pohl
-published here https://doi.org/10.5281/zenodo.4452800 as
-plots/plotstuff.py"""
+"""PlottingContext written by Christoph Pohl published here https://doi.org/10.5281/zenodo.4452800 as plots/plotstuff.py"""
+
+# ### Matplotlib customization
+# https://matplotlib.org/stable/users/explain/customizing.html#customizing-matplotlib-with-style-sheets-and-rcparams
+
 from pathlib import Path
 import subprocess
 import matplotlib as mpl
@@ -10,17 +12,33 @@ import matplotlib.pyplot as plt
 
 
 class PlottingContext:
-    def __init__(self, argv, style):
+    def __init__(self, argv: list[str], styles: list[str]):
+        """Initializes a plotting context manager.
+
+        Args:
+            argv: List of command line arguments.
+            styles: The style(s) to use. This can either be a list of known matplotlib
+            styles or stylefiles to read from.
+        """
+
         if len(argv) == 2:
             self.write_output = True
             self.filename = argv[1]
         else:
             self.write_output = False
-        if style in plt.style.available:
-            plt.style.use(style)
-        else:
-            stylefile = Path(__file__).parent / (style + ".mplstyle")
-            plt.style.use(str(stylefile))
+
+        # styles are composable
+        _styles = []
+        for style in styles:
+            if style in plt.style.available:
+                _styles.append(style)
+            else:
+                stylefile = Path(style)
+                if stylefile.exists():
+                    _styles.append(str(stylefile))
+                else:
+                    raise FileNotFoundError(f"The file {str(stylefile)} could not be found.")
+        plt.style.use(_styles)
         self.fig = plt.figure(constrained_layout=True)
 
     def __enter__(self):
